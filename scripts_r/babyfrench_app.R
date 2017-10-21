29f507984c183675197a19abadb5f0fb60fd1afd
Julia C <carbajal.mjulia@gmail.com>
2017-10-21 12:08:13 +0200

    Drop-down menu to select plot type.

--- scripts_r/babyfrench_app.R
+++ scripts_r/babyfrench_app.R
@@ -9,11 +9,19 @@ ui = fluidPage(
   textInput(inputId = 'whichword',
             label = "Select a word",
             value = 'le'),
-  plotOutput('hist')
+  selectInput("outputplot", "Type:",
+              c("Freq. per month" = "rel.freq",
+                "Cumulative freq." = "cumu.freq",
+                "Counts per month" = "wordcount",
+                "Cumulative counts" = "cumu.count")),
+  plotOutput('plot')
 )
 server = function(input, output) {
-  output$hist = renderPlot({
-    ggplot(subset(data.normalized, word == input$whichword), aes(x=age,y=rel.freq))+geom_point()
+  output$plot = renderPlot({
+    if (!is.null(input$outputplot)) {
+      column = input$outputplot
+    }
+    ggplot(subset(data.normalized, word == input$whichword), aes_string(x='age',y=column))+geom_point()
     })
 }
 shinyApp(ui=ui, server=server)
