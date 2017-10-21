3a2300e3599e7a430b898028e20c8c53bcef6e00
Julia C <carbajal.mjulia@gmail.com>
2017-10-21 12:23:23 +0200

    Aesthetic improvements.

--- scripts_r/babyfrench_app.R
+++ scripts_r/babyfrench_app.R
@@ -5,10 +5,11 @@ library(ggplot2)
 
 source('scripts_r/preprocessing.R')
 
+# User Interface
 ui = fluidPage(
   textInput(inputId = 'whichword',
             label = "Select a word",
-            value = 'le'),
+            value = 'maman'),
   selectInput("outputplot", "Type:",
               c("Freq. per month" = "rel.freq",
                 "Cumulative freq." = "cumu.freq",
@@ -16,13 +17,25 @@ ui = fluidPage(
                 "Cumulative counts" = "cumu.count")),
   plotOutput('plot')
 )
+
+# Server
 server = function(input, output) {
   output$plot = renderPlot({
-    if (!is.null(input$outputplot)) {
-      column = input$outputplot
+    # Note: replace this with a table with
+    # the correct ylabel for each plot type
+    if (input$outputplot == 'rel.freq') {
+      ytitle = 'Counts per 1000 words'
+    } else {
+      ytitle = input$outputplot
     }
-    ggplot(subset(data.normalized, word == input$whichword), aes_string(x='age',y=column))+geom_point()
-    })
+    # Plot
+    ggplot(subset(data.normalized, word == input$whichword), aes_string(x='age', y=input$outputplot)) +
+      geom_point(size=2)  +
+      xlab('Age (months)') +
+      ylab(ytitle) +
+      theme(text = element_text(size=20))
+  })
 }
+
 shinyApp(ui=ui, server=server)
 
