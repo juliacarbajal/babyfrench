c9a82cbf48c9bd14cd134b40d81117ef37fc1806
Julia C <carbajal.mjulia@gmail.com>
2017-10-23 12:13:19 +0200

    Added description, allowed spaces before words.

--- scripts_r/babyfrench_app.R
+++ scripts_r/babyfrench_app.R
@@ -11,32 +11,41 @@ font_import(pattern="[U/u]buntu")
 loadfonts(device="win")
 
 # User Interface
-ui = navbarPage(title = "BabyFrench Word Database",
-                theme = shinytheme("united"),
-  tabPanel('Word frequencies',
-  br(),
-  fluidRow(
-    column(3,
-           textInput(inputId = 'wordselect',
-                     label = "Type word(s) to search:",
-                     value = 'maman'),
-           selectInput("outputplot", "Output measure:",
-                       c("Freq. per month" = "rel.freq",
-                         "Cumulative freq." = "cumu.freq",
-                         "Counts per month" = "wordcount",
-                         "Cumulative counts" = "cumu.count")),
-           actionButton(inputId = "go",
-                        label = "Search!")
-           ),
-    column(9, plotOutput('plot'))
-  )
-  )
+ui = navbarPage(
+       title = "BabyFrench Word Database",
+       theme = shinytheme("united"),
+       tabPanel(
+         'Word frequencies',
+         h1('Word Frequencies'),
+         hr(),
+         div(p('This database contains word frequencies calculated over a compilation of French CHILDES corpora.
+         The compiled corpus is composed of speech transcripts from adults speaking to or near a child.\n
+         Age range of children included in these corpora: 11m - 24m.'),
+             p('To search the database, type one or more French words (separated by commas).')),
+         br(),
+         fluidRow(
+           column(3,
+              textInput(inputId = 'wordselect',
+                        label = "Type word(s) to search:",
+                        value = 'maman,bébé'),
+              selectInput("outputplot", "Output measure:",
+                          c("Freq. per month"   = "rel.freq",
+                            "Cumulative freq."  = "cumu.freq",
+                            "Counts per month"  = "wordcount",
+                            "Cumulative counts" = "cumu.count")),
+              actionButton(inputId = "go",
+                           label = "Search!")
+            ),
+            column(9, plotOutput('plot'))
+         )
+        )
 )
 
 # Server
 server = function(input, output) {
   wordlist = eventReactive(input$go, {
-    whichwords = strsplit(input$wordselect,',')[[1]]
+    whichwords = gsub(' ','',input$wordselect)
+    whichwords = strsplit(whichwords,',')[[1]]
   })
   output$plot = renderPlot({
     # Separate input words
